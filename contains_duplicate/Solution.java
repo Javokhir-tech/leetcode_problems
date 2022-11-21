4080170a0513eae46c1323c44fc0baaf417f11be
Javokhir-tech <abdullaev.dzhavokhir@mail.ru>
2022-11-22 00:10:19 +0500

    217. contains duplicate

---
+++ contains_duplicate/Solution.java
@@ -0,0 +1,21 @@
+package contains_duplicate;
+
+import java.util.HashMap;
+
+/*
+ * Given an array of integers nums and an integer target,
+ * return true if there's a repeated num in arr
+ * return false if all elements are distinct
+ *
+ * */
+public class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        var map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]))
+                return true;
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
