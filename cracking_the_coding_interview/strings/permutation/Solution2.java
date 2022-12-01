f583cac73427477cdfd6a7eb3f2b1ca8c2d88af4
Javokhir-tech <abdullaev.dzhavokhir@mail.ru>
2022-12-01 21:13:02 +0500

    ctci: 1.2 isPermutation optimized runtime

--- cracking_the_coding_interview/strings/permutation/Solution2.java
+++ cracking_the_coding_interview/strings/permutation/Solution2.java
@@ -3,27 +3,26 @@ package cracking_the_coding_interview.strings.permutation;
 import java.util.Arrays;
 
 /*
-* check if 2 given strs are permutation to other
-* sorts strs then compares results
-* time O(nlogn)
-* space O(1)
+* the same problem but with optimized solution in terms of runtime
+* time O(n)
+* space O(n)
 * */
-public class Solution {
-
+public class Solution2 {
     private static boolean isPermutation(String a, String b) {
         if (a.length() != b.length()) return false;
         var arrA = sortArrays(a);
         var arrB = sortArrays(b);
         return Arrays.equals(arrA, arrB);
     }
-
-    private static char[] sortArrays(String str) {
-        var arr = str.toCharArray();
-        Arrays.sort(arr);
+    private static int[] sortArrays(String str) {
+        var arr = new int[26];
+        for (char c : str.toCharArray()) {
+            arr[c - 'a']++;
+        }
         return arr;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPermutation("abkac", "kcbaa"));
+        System.out.println(isPermutation("kabkacs", "kkcbsaa"));
     }
 }
