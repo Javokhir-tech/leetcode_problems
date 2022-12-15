5736d305b3b17bc75adb93b4147f614e1782c8d3
Javokhir-tech <abdullaev.dzhavokhir@mail.ru>
2022-12-16 00:58:49 +0500

    ctci: 1.5 sumlist

--- cracking_the_coding_interview/linked_list/theory/Recursiv.java
+++ cracking_the_coding_interview/linked_list/theory/Recursiv.java
@@ -4,12 +4,21 @@ import static cracking_the_coding_interview.linked_list.theory.Recursiv.Node.*;
 
 public class Recursiv {
     public static void main(String[] args) {
-        var node = new Node(2);
-        insert(node, 3);
-        insert(node, 89);
-        insert(node, 5);
-        insertRecursive(node,12);
-        showRecursive(node);
+//        var node = new Node(2);
+//        insert(node, 3);
+//        insert(node, 89);
+//        insert(node, 5);
+//        insertRecursive(node,12);
+//        showRecursive(node);
+
+        var node1 = new Node(6);
+        insert(node1, 1);
+        insert(node1, 7);
+        var node2 = new Node(2);
+        insert(node2, 9);
+        insert(node2, 5);
+
+        show(sumList(node1, node2));
     }
 
     static class Node {
@@ -17,6 +26,8 @@ public class Recursiv {
         Node next;
 
         public Node(int data) {this.data = data;}
+
+        public Node() {}
 
         public int getData() {
             return data;
@@ -58,6 +69,33 @@ public class Recursiv {
             if (head == null) return;
             System.out.print(head.data + " -> ");
             showRecursive(head.next);
+        }
+
+        static Node sumList(Node list1, Node list2) {
+            var str1 = new StringBuilder();
+            var str2 = new StringBuilder();
+
+            while (list1 != null) {
+                str1.append(list1.data);
+                list1 = list1.next;
+            }
+
+            while (list2 != null) {
+                str2.append(list2.data);
+                list2 = list2.next;
+            }
+
+            var int1 = Integer.valueOf(str1.toString());
+            var int2 = Integer.valueOf(str2.toString());
+
+            var int3 = int1 + int2;
+
+            var arr = String.valueOf(int3).split("");
+            var result = new Node(Integer.parseInt(arr[arr.length - 1]));
+            for (int i = arr.length - 2; i >= 0; i--)
+                insert(result, Integer.parseInt(arr[i]));
+
+            return result;
         }
     }
 }
