c85268a26fa2d77a35804ed88c293eac96e60ee8
JavokhirAbdullaev <jabdullaev@ebsco.com>
2023-01-06 12:57:22 +0500

    update

--- cracking_the_coding_interview/stack_n_queues/Main.java
+++ cracking_the_coding_interview/stack_n_queues/Main.java
@@ -3,6 +3,7 @@ package cracking_the_coding_interview.stack_n_queues;
 public class Main {
     public static void main(String[] args) {
         var stack = new Stack<Integer>();
+        System.out.println(stack.isEmpty());
         stack.push(2);
         stack.push(5);
         stack.push(3);
@@ -16,5 +17,6 @@ public class Main {
         queue.add(9);
         queue.add(6);
         queue.print();
+        System.out.println(stack.isEmpty());
     }
 }
--- cracking_the_coding_interview/stack_n_queues/Stack.java
+++ cracking_the_coding_interview/stack_n_queues/Stack.java
@@ -2,6 +2,9 @@ package cracking_the_coding_interview.stack_n_queues;
 
 import java.util.EmptyStackException;
 
+/*
+* internally uses linked list
+* */
 public class Stack<T> {
     public static class StackNode<T> {
         private T data;
@@ -14,13 +17,19 @@ public class Stack<T> {
 
     private StackNode<T> top;
 
+    /*
+    * removes from top
+    * */
     public T pop() {
+        if (top == null) throw new EmptyStackException();
         var data = top.data;
         top = top.next;
-        if (top == null) throw new EmptyStackException();
         return data;
     }
 
+    /*
+    * adds to top
+    * */
     public void push(T item) {
         var t = new StackNode<T>(item);
         t.next = top;
@@ -33,5 +42,20 @@ public class Stack<T> {
             System.out.println(t.data);
             t = t.next;
         }
+    }
+
+    /*
+    * checks if empty
+    * */
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    /*
+    * returns top element
+    * */
+    public T peek() {
+        if (top == null) throw new EmptyStackException();
+        return top.data;
     }
 }
