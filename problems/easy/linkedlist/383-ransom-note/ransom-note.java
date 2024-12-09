53485396790e16b8de3729cfa754ca3e7b18f73f
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2024-12-09 23:14:35 +0500

    Time: 10 ms (60.65%) | Memory: 45.4 MB (15.69%) - LeetSync

--- problems/easy/linkedlist/383-ransom-note/ransom-note.java
+++ problems/easy/linkedlist/383-ransom-note/ransom-note.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<>();
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>(); 
+
         for (char c : magazine.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
