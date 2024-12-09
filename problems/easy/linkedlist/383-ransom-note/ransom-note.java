6c72b5b317b047b7f5c6311c4e16a194073b4d32
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2024-12-09 23:12:00 +0500

    Time: 11 ms (46.42%) | Memory: 45.4 MB (8.99%) - LeetSync

---
+++ problems/easy/linkedlist/383-ransom-note/ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            int currentCount = map.getOrDefault(c, 0);
+
+            if (currentCount == 0) {
+                return false;
+            }
+            map.put(c, currentCount - 1);
+        }
+        return true;
+    }
+}
