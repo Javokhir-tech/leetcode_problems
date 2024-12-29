9d1a975a2db19f4a8d5274cd2531640ffda6b197
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2024-12-29 15:06:05 +0500

    Time: 12 ms (30.40%) | Memory: 45.7 MB (26.40%) - LeetSync

---
+++ problems/easy/string/271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -0,0 +1,24 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str);
+            sb.append("π");
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        System.out.println(s);
+        String[] strs = s.split("π", -1);
+
+        return Arrays.asList(strs).subList(0, strs.length - 1);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
