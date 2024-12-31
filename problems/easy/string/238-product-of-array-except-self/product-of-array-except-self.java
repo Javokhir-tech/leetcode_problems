89484ba6feb2624db1c004c4765f5f988cdedd5e
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2024-12-31 22:54:39 +0500

    Time: 2 ms (89.33%) | Memory: 56.4 MB (10.58%) - LeetSync

--- problems/easy/string/238-product-of-array-except-self/product-of-array-except-self.java
+++ problems/easy/string/238-product-of-array-except-self/product-of-array-except-self.java
@@ -12,12 +12,10 @@ class Solution {
         prefix[0] = 1;
         for (int i = 1; i < nums.length; i++) {
             prefix[i] = prefix[i - 1] * nums[i - 1];
-            System.out.println("p " + prefix[i]);
         }
         suffix[nums.length - 1] = 1;
         for (int i = nums.length - 2; i >= 0; i--) {
             suffix[i] = suffix[i + 1] * nums[i + 1];
-            System.out.println("s " + suffix[i]);
         }
         for (int i = 0; i < nums.length; i++) {
             answer[i] = prefix[i] * suffix[i];
