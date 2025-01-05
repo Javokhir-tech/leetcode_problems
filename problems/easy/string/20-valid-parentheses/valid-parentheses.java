149ef2a4e1a16e53e66545b4dceead3c9d4c9c85
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2025-01-06 00:41:15 +0500

    Time: 3 ms (41.50%) | Memory: 42.2 MB (11.03%) - LeetSync

--- problems/easy/string/20-valid-parentheses/valid-parentheses.java
+++ problems/easy/string/20-valid-parentheses/valid-parentheses.java
@@ -1,16 +1,19 @@
 class Solution {
 
-    private static final Map<Character, Character> maps = Map.of('{', '}', '[', ']', '(', ')');    
+    private static final Map<Character, Character> maps = 
+    Map.of('}', '{', 
+           ']', '[', 
+           ')', '(');    
 
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (char ch : s.toCharArray()) {
-            if (maps.containsKey(ch)) {
-                stack.push(maps.get(ch));
+            if (maps.containsValue(ch)) { // if opening
+                stack.push(ch);
             }
             else {
-                if (stack.isEmpty() || stack.pop() != ch) {
+                if (stack.isEmpty() || stack.pop() != maps.get(ch)) {
                     return false;
                 }
             }
