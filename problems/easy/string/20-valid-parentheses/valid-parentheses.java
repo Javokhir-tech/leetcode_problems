820492a6c0810dc3c0431cef8e1a26120fb2d3d4
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2025-01-05 23:55:37 +0500

    Time: 4 ms (15.42%) | Memory: 42 MB (29.08%) - LeetSync

--- problems/easy/string/20-valid-parentheses/valid-parentheses.java
+++ problems/easy/string/20-valid-parentheses/valid-parentheses.java
@@ -4,9 +4,6 @@ class Solution {
     Map.of('{', '}', '[', ']', '(', ')');    
 
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) {
-            return false;
-        }
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
