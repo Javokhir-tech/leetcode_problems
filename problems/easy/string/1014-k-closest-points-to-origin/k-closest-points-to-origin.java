9c1b9d8fe2fb48845b1ecb96e977c90f99b05cca
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2025-01-26 17:03:03 +0500

    Time: 31 ms (56.82%) | Memory: 55.2 MB (24.14%) - LeetSync

--- problems/easy/string/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ problems/easy/string/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,22 +1,23 @@
 class Solution {
     private class EuclideanDistance {
-        private double distance;
+        private int distance;
         private int[] coordinates;
-        private EuclideanDistance(double distance, int[] coordinates) {
+        private EuclideanDistance(int distance, int[] coordinates) {
             this.distance = distance;
             this.coordinates = coordinates;
         }
     }
 
-    private double calculateDistance(int[] pairs) {
-        return Math.sqrt(Math.pow(pairs[0], 2) + Math.pow(pairs[1], 2));
+    private int calculateDistance(int[] pairs) {
+        return (int)(Math.pow(pairs[0], 2) + Math.pow(pairs[1], 2));
     }
 
     public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<EuclideanDistance> maxHeap = new PriorityQueue<>(
-            (o1, o2) -> Double.compare(o2.distance, o1.distance));
+            (o1, o2) -> o2.distance - o1.distance);
+
         for (int[] pairs : points) {
-            double distance = calculateDistance(pairs);
+            int distance = calculateDistance(pairs);
             maxHeap.add(new EuclideanDistance(distance, pairs));
             if (maxHeap.size() > k) {
                 maxHeap.poll(); // remove biggest
