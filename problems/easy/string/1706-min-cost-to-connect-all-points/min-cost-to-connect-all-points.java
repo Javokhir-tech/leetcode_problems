fd342a4b627d4ad76ee2c233982f2346969a7b4f
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2025-04-05 18:49:07 +0500

    Time: 125 ms (63.84%) | Memory: 62.6 MB (47.51%) - LeetSync

---
+++ problems/easy/string/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int minCostConnectPoints(int[][] points) {
+        Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]); // cost, pointIdx
+        int n = points.length;
+        Set<Integer> visited = new HashSet<>();
+        minHeap.add(new int[]{0, 0});
+        // visited.add(0);
+        int totalCost = 0;
+        int nodesVisited = 0;
+        while (nodesVisited < n) {
+            int[] curr = minHeap.poll();
+            int cost = curr[0], idx = curr[1];
+            if (visited.contains(idx)) {
+                continue;
+            }
+            visited.add(idx);
+            totalCost += cost;
+            nodesVisited++;
+            for (int i = 0; i < n; i++) {
+                if (!visited.contains(i)) {
+                    int dist = calculateDistance(points[idx], points[i]);
+                    minHeap.add(new int[]{dist, i});
+                }
+            }
+        }
+        return totalCost;
+    }
+
+    private int calculateDistance(int[] p1, int[] p2) {
+        return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+    }
+}
