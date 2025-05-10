7ba7c8c75dd96fb493140d03d2ca8e5024625fc0
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2025-05-11 00:06:20 +0500

    Time: 59 ms (33.99%) | Memory: 73.2 MB (39.28%) - LeetSync

--- problems/easy/string/474-ones-and-zeroes/ones-and-zeroes.java
+++ problems/easy/string/474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,8 +1,13 @@
 class Solution {
     int[][][] dp;
-
+    int[][] arr;
     public int findMaxForm(String[] strs, int m, int n) {
-       // include or not include str
+       arr = new int[strs.length][2];
+       for (int i = 0; i < strs.length; i++) {
+            for (char c : strs[i].toCharArray()) {
+                arr[i][c - '0']++;
+            }
+       } 
        dp = new int[strs.length][m + 1][n + 1];
        for (int i = 0; i < strs.length; i++) {
             for (int j = 0; j <= m; j++) {
@@ -11,11 +16,11 @@ class Solution {
                 }
             }
         }
-       return dfs(0, m, n, strs); 
+       return dfs(0, m, n); 
     }
 
-    private int dfs(int i, int m, int n, String[] strs) {
-        if (i == strs.length) {
+    private int dfs(int i, int m, int n) {
+        if (i == arr.length) {
             return 0;
         }
 
@@ -23,12 +28,12 @@ class Solution {
             return dp[i][m][n];
         }
 
-        dp[i][m][n] = dfs(i + 1, m, n, strs);
-        int mCnt = (int) strs[i].chars().filter(c -> c == '0').count(); // count 0s
-        int nCnt = (int) strs[i].chars().filter(c -> c == '1').count(); // count 1
+        dp[i][m][n] = dfs(i + 1, m, n);
+        int mCnt = arr[i][0]; // count 0s
+        int nCnt = arr[i][1]; // count 1
         if (mCnt <= m && nCnt <= n) {
-            dp[i][m][n] = Math.max(dfs(i + 1, m, n, strs), 
-                1 + dfs(i + 1, m - mCnt, n - nCnt, strs)
+            dp[i][m][n] = Math.max(dfs(i + 1, m, n), 
+                1 + dfs(i + 1, m - mCnt, n - nCnt)
             );
         }
         return dp[i][m][n];
