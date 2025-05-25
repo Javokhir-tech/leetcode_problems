9bd3768d34fe21848f819b3058b54d98fc5d0028
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2025-05-25 12:43:41 +0500

    Time: 14 ms (86.62%) | Memory: 44.4 MB (65.32%) - LeetSync

--- problems/easy/string/322-coin-change/coin-change.java
+++ problems/easy/string/322-coin-change/coin-change.java
@@ -1,31 +1,17 @@
 class Solution {
-    Map<Integer, Integer> cache;
-    public int coinChange(int[] coins, int amount) {
-        this.cache = new HashMap<>();
-        int minCoin = dfs(coins, amount);
-        return (minCoin >= 1e9) ? -1 : minCoin; 
-    }
 
-    private int dfs(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
-        else if (amount < 0) {
-            return Integer.MAX_VALUE;
-        }
-        if (cache.containsKey(amount)) {
-            return cache.get(amount);
-        }
-        int res = (int) 1e9;
-        for (int coin : coins) {
-            if (amount - coin >= 0) {
-                int result = dfs(coins, amount - coin);
-                if (result != Integer.MAX_VALUE) {
-                    res = Math.min(1 + result, res);
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;  // only one way to sum up 0
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
                 }
             }
         }
-        cache.put(amount, res);
-        return res;
+        return dp[amount] >= amount + 1 ? -1 : dp[amount];
     }
 }
