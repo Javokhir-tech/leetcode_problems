c6d398b3d489bb129f3d4f4d0eb6f8b9681717b1
Javokhir Abdullaev <62796028+Javokhir-tech@users.noreply.github.com>
2025-05-25 12:05:51 +0500

    Time: 157 ms (5.81%) | Memory: 46.4 MB (5.80%) - LeetSync

--- problems/easy/string/322-coin-change/coin-change.java
+++ problems/easy/string/322-coin-change/coin-change.java
@@ -3,21 +3,23 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         this.cache = new HashMap<>();
         int minCoin = dfs(coins, amount);
-        return (minCoin >= 1e9) ? -1 : minCoin;
+        return (minCoin >= 1e9) ? -1 : minCoin; 
     }
 
     private int dfs(int[] coins, int amount) {
-        if (amount == 0) return 0;
-
+        if (amount == 0) {
+            return 0;
+        }
+        else if (amount < 0) {
+            return Integer.MAX_VALUE;
+        }
         if (cache.containsKey(amount)) {
             return cache.get(amount);
         }
-
-        int res = (int)1e9;
+        int res = (int) 1e9;
         for (int coin : coins) {
             if (amount - coin >= 0) {
-                res = Math.min(1 + dfs(coins, amount - coin), 
-                    res);
+                res = Math.min(1 + dfs(coins, amount - coin), res);
             }
         }
         cache.put(amount, res);
